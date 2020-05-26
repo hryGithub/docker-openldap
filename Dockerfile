d71f9bf05013750d35c7a29cbe8ed81ec0b4b825
hujian <hujian@tigerft.com>
2020-05-26 15:10:28 +0800

    更新

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,11 @@ RUN echo -e "http://mirrors.aliyun.com/alpine/v3.11/main\nhttp://mirrors.aliyun.
 RUN apk update && apk add --no-cache bash gettext openldap openldap-clients openldap-back-mdb openldap-overlay-memberof openldap-overlay-ppolicy openldap-overlay-refint
 RUN mv -vf /etc/openldap/ldap.conf /etc/openldap/ldap.conf.original && mv -vf /etc/openldap/slapd.conf /etc/openldap/slapd.conf.original
 
+ENV LAPD_ORGANIZATION=example \
+    LDAP_DOMAIN=example.org \
+    LDAP_ROOTPW=admin
+
+
 EXPOSE 389 636
 
 COPY *.template /srv/openldap/
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,46 +1,27 @@
 #!/bin/sh -e
+OPENLDAP_CONFIG_DIR="/etc/openldap/slapd.d"
+OPENLDAP_ULIMIT="2048"
 
-export OPENLDAP_ETC_DIR="/etc/openldap"
-export OPENLDAP_RUN_DIR="/run/openldap"
-export OPENLDAP_RUN_ARGSFILE="${OPENLDAP_RUN_DIR}/slapd.args"
-export OPENLDAP_RUN_PIDFILE="${OPENLDAP_RUN_DIR}/slapd.pid"
-export OPENLDAP_MODULES_DIR="/usr/lib/openldap"
-export OPENLDAP_CONFIG_DIR="${OPENLDAP_ETC_DIR}/slapd.d"
-export OPENLDAP_BACKEND_DIR="/var/lib/openldap"
-export OPENLDAP_BACKEND_DATABASE="mdb"
-export OPENLDAP_BACKEND_OBJECTCLASS="olcMdbConfig"
-export OPENLDAP_ULIMIT="2048"
-
-export LDAP_DOMAIN="${LDAP_DOMAIN:-example.com}"
-export LDAP_DOMAIN_RDC="$(echo ${LDAP_DOMAIN} | sed 's/^\.//; s/\..*$//')"
-# export LDAP_DOMAIN_OBJECTCLASS="organization
-# objectClass: dcObject
-# "
-export LDAP_DOMAIN_OBJECTCLASS="${LDAP_DOMAIN_OBJECTCLASS:-domain}"
-export LDAP_ORGANIZATION="${LDAP_ORGANIZATION:-${LDAP_DOMAIN}}"
-export LDAP_SUFFIX="${LDAP_SUFFIX:-$(echo dc=$(echo ${LDAP_DOMAIN} | sed 's/^\.//; s/\./,dc=/g'))}"
-export LDAP_PASSWORD="${LDAP_PASSWORD:-lderp!${LDAP_DOMAIN_RDC}}"
-export LDAP_PASSWORD_ENCRYPTED="$(slappasswd -u -h '{SSHA}' -s ${LDAP_PASSWORD})"
+LDAP_SUFFIX="dc=${LDAP_DOMAIN//./,dc=}"
+LDAP_PASSWORD_ENCRYPTED="$(slappasswd -u -h '{SSHA}' -s ${LDAP_PASSWORD})"
 
 ulimit -n ${OPENLDAP_ULIMIT}
-mkdir -p ${OPENLDAP_RUN_DIR} /srv/openldap.d && chown -R ldap.ldap ${OPENLDAP_RUN_DIR}
+mkdir -p /var/run/openldap /srv/openldap.d && chown -R ldap.ldap /var/run/openldap
 
 if [[ ! -d ${OPENLDAP_CONFIG_DIR}/cn=config ]]; then
     mkdir -p ${OPENLDAP_CONFIG_DIR}
 
-    if [[ ! -s ${OPENLDAP_ETC_DIR}/slapd-config.ldif ]]; then
-        cat /srv/openldap/slapd-config.ldif.template | envsubst > ${OPENLDAP_ETC_DIR}/slapd-config.ldif
+    if [[ ! -s /etc/openldap/slapd-config.ldif ]]; then
+        cat /srv/openldap/slapd-config.ldif.template | envsubst > /etc/openldap/slapd-config.ldif
     fi
 
-    slapadd -n0 -F ${OPENLDAP_CONFIG_DIR} -l ${OPENLDAP_ETC_DIR}/slapd-config.ldif > ${OPENLDAP_ETC_DIR}/slapd-config.ldif.log
+    slapadd -n0 -F ${OPENLDAP_CONFIG_DIR} -l /etc/openldap/slapd-config.ldif > /etc/openldap/slapd-config.ldif.log
 
-    if [[ ! -s ${OPENLDAP_ETC_DIR}/ldap.conf ]]; then
-        cat /srv/openldap/ldap.conf.template | envsubst > ${OPENLDAP_ETC_DIR}/ldap.conf
+    if [[ ! -s /etc/openldap/ldap.conf ]]; then
+        cat /srv/openldap/ldap.conf.template | envsubst > /etc/openldap/ldap.conf
     fi
 
-    mkdir -p ${OPENLDAP_BACKEND_DIR}/run
-    chown -R ldap:ldap ${OPENLDAP_BACKEND_DIR}
-    chown -R ldap:ldap ${OPENLDAP_CONFIG_DIR} ${OPENLDAP_BACKEND_DIR}
+    chown -R ldap:ldap ${OPENLDAP_CONFIG_DIR} 
 
     if [[ -d /srv/openldap.d ]]; then
         if [[ ! -s /srv/openldap.d/000-domain.ldif ]]; then
@@ -59,15 +40,15 @@ if [[ ! -d ${OPENLDAP_CONFIG_DIR}/cn=config ]]; then
             esac
         done
 
-        if [[ ! -s ${OPENLDAP_RUN_PIDFILE} ]]; then
-            echo >&2 "$0 ($slapd_exe): ${OPENLDAP_RUN_PIDFILE} is missing, did the daemon start?"
+        if [[ ! -s /var/run/openldap/slapd.pid ]]; then
+            echo >&2 "$0 ($slapd_exe): /var/run/openldap/slapd.pid is missing, did the daemon start?"
             exit 1
         else
-            slapd_pid=$(cat ${OPENLDAP_RUN_PIDFILE})
+            slapd_pid=$(cat /var/run/openldap/slapd.pid)
             echo >&2 "$0 ($slapd_exe): sending SIGINT to initdb daemon with pid=$slapd_pid"
             kill -s INT "$slapd_pid" || true
             while : ; do
-                [[ ! -f ${OPENLDAP_RUN_PIDFILE} ]] && break
+                [[ ! -f /var/run/openldap/slapd.pid ]] && break
                 sleep 1
                 echo >&2 "$0 ($slapd_exe): initdb daemon is still up, sleeping ..."
             done
