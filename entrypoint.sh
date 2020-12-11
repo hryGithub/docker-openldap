797226d4ad6da95e7b0488f87cf1cfba858b6e58
hujian <hujian@tigerft.com>
2020-12-11 14:28:52 +0800

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,17 +22,6 @@ pidfile		/run/openldap/slapd.pid
 argsfile	/run/openldap/slapd.args
 modulepath  /usr/lib/openldap
 moduleload  back_mdb.so
-#access
-access to * by self read
-            by dn="${LDAP_ROOTDN}" write 
-            by * none
-            
-access to attrs=userPassword,shadowLastChange 
-            by self write 
-            by dn="${LDAP_ROOTDN}" write   
-            by anonymous auth 
-            by * none
-#
 database mdb
 maxsize 1073741824
 suffix "${LDAP_SUFFIX}"
@@ -54,6 +43,17 @@ objectClass: organizationalRole
 cn: admin
 	EOF
 
+    cat <<-EOF > "${LDAP_CONF_DIR}/security.ldif"
+dn: olcDatabase={1}mdb,cn=config
+changetype: modify
+delete: olcAccess
+-
+add: olcAccess
+olcAccess: to * by dn.exact=gidNumber=0+uidNumber=0,cn=peercred,cn=external,cn=auth manage by * break
+olcAccess: to attrs=userPassword,shadowLastChange by self write by dn="${LDAP_ROOTDN}" write by anonymous auth by * none
+olcAccess: to * by self read by dn="${LDAP_ROOTDN}" write by * none
+	EOF
+
     # RFC2307bis schema
     if [ "${LDAP_RFC2307BIS_SCHEMA}" == "true" ]; then
         sed -i "s@nis.schema@rfc2307bis.schema@g" $LDAP_CONF
@@ -62,7 +62,8 @@ cn: admin
     echo "Generating configuration"
     slaptest -f ${LDAP_CONF} -F ${LDAP_CONF_DIR} -d ${LDAP_LOGLEVE}
     slapadd  -c -F ${LDAP_CONF_DIR}  -l "${LDAP_CONF_DIR}/base.ldif" 
-        
+    slapadd  -c -F ${LDAP_CONF_DIR}  -l "${LDAP_CONF_DIR}/security.ldif"
+    
     chown -R ldap:ldap ${LDAP_CONF_DIR} /var/run/openldap /var/lib/openldap
 
 fi
